edc0780ba7d0c09afd2bc6239b58d499ad77ac31
Ludvik Jerabek <ljerabek@proofpoint.com>
2023-02-09 11:28:42 -0500

    Add modernizations

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
-setup(
-    name='et-api-python',
-    version='0.1.0',
-    packages=['et_api', 'et_api.resources', 'et_api.endpoints'],
-    install_requires=['requests'],
-    url='https://github.com/pfptcommunity/et-api-python',
-    license='MIT',
-    author='Ludvik Jerabek',
-    author_email='',
-    description='Proofpoint Security Awareness Training Python API Package'
-)
+if __name__ == "__main__":
+    setup()
